edcb1de617193486d5fdcaeb1000af9f8864c99d
John Eriksson <johnreriksson@gmail.com>
2014-01-02 19:02:05 +0100

    Started work

---
+++ wp-content/themes/nn/page-home.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+	<title><?php wp_title(); ?></title>
+
+	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/foundation.min.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
