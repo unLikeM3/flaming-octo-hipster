7f0bd9215a2ccd67212373218ad5052e6f6004a0
John Eriksson <johnreriksson@gmail.com>
2014-01-02 20:50:21 +0100

    Scaffolding

--- wp-content/themes/nn/page-home.php
+++ wp-content/themes/nn/page-home.php
@@ -1,3 +1,9 @@
+<?php 		
+	/*
+		Template Name: Home
+	*/
+ ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,6 +17,56 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/foundation.min.js"></script>
 </head>
 <body>
-	
+	<header>
+		<div class="small-4 columns">
+			
+		</div>
+		<div class="small-8 columns">
+			<nav>
+				<?php wp_nav_menu(); ?>
+			</nav>
+		</div>
+	</header>
+	<section class="row spacer hide-for-small"><img src="http://www.placehold.it/1000x300" alt="" class="bigimage"></section>
+	<section class="margin-top">
+		<div class="row content">
+			<div class="large-8 columns">
+				<h1>Title</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, assumenda, reiciendis, sint ipsam reprehenderit quis labore aut officiis neque ratione dolor magnam ea dolorem asperiores at corporis recusandae blanditiis tempore delectus pariatur. Dolor, voluptatem, blanditiis non illum ratione laboriosam assumenda nemo natus maiores ad error culpa sint id unde accusantium velit quisquam a delectus. In, eaque, earum nobis iusto sint soluta ipsum amet commodi alias obcaecati animi praesentium omnis dolores doloribus quisquam incidunt odit ducimus quam quidem voluptatum ullam officia doloremque facilis. Vitae, aspernatur dignissimos quam similique voluptas laudantium aliquam nostrum nam inventore sunt dolores aliquid tempora qui nesciunt unde.</p>
+			</div>
+			<div class="large-4 columns">
+				<h1>Title2</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, quaerat, corporis eaque amet culpa voluptates iste officia esse molestias harum. Assumenda, dolor, sed, doloremque, omnis blanditiis corporis accusamus nihil dolorem fugit architecto voluptas beatae reprehenderit nulla vel ipsam quae aspernatur?</p>
+			</div>
+		</div>
+	</section>
+	<section class="margin-top">
+		<div class="row">
+			<div class="newsitems">
+				<div class="newsitem content">
+					<h1>title</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo quas laboriosam dolores non placeat nostrum quibusdam eaque. Sit, adipisci voluptate labore saepe error debitis odit laboriosam veniam rem. Voluptate, deleniti.</p>
+				</div>
+				<div class="newsitem content">
+					<h1>title</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, maiores voluptates harum dolor ab soluta aperiam omnis? Mollitia, placeat, veniam! Sunt, temporibus, tempore, voluptate necessitatibus repellendus iure dolor adipisci nemo obcaecati delectus reprehenderit nobis numquam eligendi ad nam. Sed, quidem!</p>
+				</div>
+			</div>
+			<div class="newsitems">
+				<div class="newsitem content">
+					<h1>title</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, delectus, ex, soluta voluptatum dolore error mollitia veniam a facilis beatae accusamus autem reprehenderit libero magnam.</p>
+				</div>
+				<div class="newsitem content">
+					<h1>title</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, nobis, expedita blanditiis minus corrupti recusandae voluptas suscipit iure illum eveniet quo adipisci asperiores et doloribus earum inventore natus. Unde, dolorem, qui, tempora illo at tenetur aliquam distinctio minima itaque voluptatem cupiditate assumenda doloremque nobis amet animi dolore iure excepturi deleniti.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section>
+		
+	</section>
+	<footer></footer>
 </body>
 </html>
